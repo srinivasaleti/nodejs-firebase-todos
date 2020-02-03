699eee4989d8684d02a3a8524b2d78941b06e918
Aleti Srinivasarao <alti@AdministorsMBP5.Dlink>
2020-02-03 11:23:34 +0530

    Add get, post todos routes

--- src/Routers/todos_router.js
+++ src/Routers/todos_router.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var todoRouter = express.Router()
 
+const todoList = []
 // define the home page route
-todoRouter.get('/', function (req, res) {
-  res.send('Todos')
+todoRouter.get('/', function(req, res) {
+  res.json(todoList);
+})
+
+todoRouter.post("/", function(req, res) {
+  const todo = {
+    text: req.body.text,
+    created_at: Date.now(),
+    updated_at: Date.now(),
+    completed: false
+  }
+  todoList.push(todo)
+  res.json(todoList)
 })
 
 module.exports = todoRouter
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+
 const todoRouter = require('./Routers/todos_router')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/todo', todoRouter)
 
